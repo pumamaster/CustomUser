f11102b84706b2d8f474361b6f55a5192578456f
Alejandro González <alejandrogongis03@gmail.com>
2024-02-06 20:24:52 -0600

    Mejora del login, no puedes ir al login si ya estás logeado

--- AppUsuario/urls.py
+++ AppUsuario/urls.py
@@ -4,6 +4,6 @@ from .views import LoginView, LogoutView, welcome
 urlpatterns = [
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('welcome/', welcome, name='welcome'),
+    path('', welcome, name='welcome'),
 
 ]
--- AppUsuario/views.py
+++ AppUsuario/views.py
@@ -1,11 +1,45 @@
+from django.http.response import HttpResponse as HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.views import View
 from .forms import LoginForm
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.generic.edit import FormView
+from django.http import HttpRequest, HttpResponseRedirect
+from django.urls import reverse_lazy
 
-class LoginView(View):
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginView(FormView):
+    template_name='login.html'
+    form_class=LoginForm
+    success_url=reverse_lazy('welcome')
+
+    @method_decorator(csrf_protect)
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return super(LoginView,self).dispatch(request, *args,**kwargs)
+        
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(self.request, user)
+            return super(LoginView, self).form_valid(form)
+        else:
+            
+            form.add_error(None, "Invalid username or password.")
+            return self.form_invalid(form)
+
+'''class LoginView(View):
     def get(self, request):
         form = LoginForm()
         return render(request, 'login.html', {'form': form})
@@ -23,7 +57,7 @@ class LoginView(View):
                 error_message = "Nombre de usuario o contraseña incorrectos."
                 return render(request, 'login.html', {'form': form, 'error_message': error_message})
         else:
-            return render(request, 'login.html', {'form': form})
+            return render(request, 'login.html', {'form': form})'''
 
 class LogoutView(View):
     def get(self, request):
